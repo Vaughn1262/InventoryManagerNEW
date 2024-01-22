f2af388b27f9aa0f875afad28162a15f6c377701
Vaughn1262 <vaughnpalmer62@gmail.com>
2024-01-22 10:32:33 -0700

    bruh

--- InventoryManagerNEW.cpp
+++ InventoryManagerNEW.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World! your mama gay\n";
+    std::cout << "Hello World! your mama gay ye\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
