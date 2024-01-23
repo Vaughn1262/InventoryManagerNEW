3fdd49fc75cc9c85e31d39116736ac60af380c49
Vaughn1262 <vaughnpalmer62@gmail.com>
2024-01-22 17:50:34 -0700

    More loop work

--- InventoryManagerNEW.cpp
+++ InventoryManagerNEW.cpp
@@ -3,24 +3,50 @@
 
 #include <iostream>
 using namespace std;
+#include <vector>
+
 
 int main()
 {
     while (true) {
-
-
         cout << "Inventory Manager: Please select an option\n";
         int menuanswer;
+        string item;
+        int quantity;
+        int value;
+        vector<string> names;
+        vector<int> quantities;
+        vector<int> values;
         cout << "1. Add an item\n2. Drop an item\n3. View Inventory\n4. Quick Sell an item\n5.Exit\n"; // Type a number and press enter
         cin >> menuanswer; // Get user input from the keyboard
         switch (menuanswer) {
         case 1:
-            cout << "1.epic\n";
-            cout << "1.epic\n";
+        {
+            cout << "What is the name of the item you would like to add?\n";
+            cin >> item;
+            cout << item;
+            cout << "How many did you pick up?\n";
+            cin >> quantity;
+            cout << "How much is it worth?\n";
+            cin >> value;
+            names.emplace(names.begin(), item);
+            cout << names[0];
+            quantities.emplace_back(quantity);
+            values.emplace_back(value);
             break;
+        }
+
         case 2:
             break;
         case 3:
+            cout << "Inventory contains: \n";
+            cout << names.size();
+            for (int i = 0; i < names.size(); i++) {
+                cout << "\n";
+                cout << names[i];
+            }
+
+
             break;
         case 4:
             break;
